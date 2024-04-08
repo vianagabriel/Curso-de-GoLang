336aad9fcc192ed4d6e419f16e5e3a2042c969a2
gabrielviana <vianagabrieldacunha@gmail.com>
2024-04-08 19:37:15 -0300

    função de editar uma tarefa criada, e correções

--- Pacotes_Importantes/Crud/main.go
+++ Pacotes_Importantes/Crud/main.go
@@ -23,9 +23,11 @@ func main() {
 		fmt.Println("Selecione uma opção:")
 		fmt.Println("1. Criar tarefa")
 		fmt.Println("2. Listar tarefas")
-		fmt.Println("3. Listar uma tarefas")
+		fmt.Println("3. Listar uma tarefa")
 		fmt.Println("4. Excluir tarefa")
-		fmt.Println("5. Sair")
+		fmt.Println("5. Editar tarefa")
+		fmt.Println("6. Sair")
+		println()
 		fmt.Print("Opção: ")
 
 		opcao := lerOpcao()
@@ -40,6 +42,8 @@ func main() {
 		case 4:
 			removeTarefa(&listaDeTarefas)
 		case 5:
+			editaTarefa(&listaDeTarefas)
+		case 6:
 			fmt.Println("Saindo...")
 			return
 		default:
@@ -56,18 +60,35 @@ func lerOpcao() int {
 	return opcao
 }
 
-// Create
+func formataApresentecao(tarefa *Tarefa) {
+	fmt.Printf("ID: %d\n", tarefa.id)
+	fmt.Printf("Nome: %s\n", tarefa.nome)
+	fmt.Printf("Descrição: %s\n", tarefa.descricao)
+	fmt.Printf("Data de criação: %s\n", tarefa.dataDeCriacao.Format("02/01/2006"))
+	fmt.Println("----------------------------------------")
+	println()
+}
+
+// ======================================================C.R.U.D.==================================================================
+
 func criarTarefa(listaDeTarefas *[]Tarefa) {
+	println()
+
 	fmt.Println("Criando uma nova tarefa:")
+	println("============================")
 	leitor := bufio.NewReader(os.Stdin)
 
 	fmt.Print("Digite o nome da tarefa: ")
 	nome, _ := leitor.ReadString('\n')
 	nome = strings.TrimSpace(nome)
 
+	println("===========================")
+
 	fmt.Print("Digite a descrição da tarefa: ")
 	descricao, _ := leitor.ReadString('\n')
 	descricao = strings.TrimSpace(descricao)
+
+	println("===========================")
 
 	novaTarefa := Tarefa{
 		id:            len(*listaDeTarefas) + 1,
@@ -79,46 +100,106 @@ func criarTarefa(listaDeTarefas *[]Tarefa) {
 	*listaDeTarefas = append(*listaDeTarefas, novaTarefa)
 
 	fmt.Println("Tarefa criada com sucesso!")
+	println()
 }
 
-// Read All
 func listarTarefas(listaDeTarefas []Tarefa) {
 	if len(listaDeTarefas) == 0 {
 		fmt.Println("Não há tarefas cadastradas.")
 		return
 	}
 
+	println()
 	fmt.Println("Listando tarefas:")
+	println()
+
 	for _, tarefa := range listaDeTarefas {
-		fmt.Printf("ID: %d\n", tarefa.id)
-		fmt.Printf("Nome: %s\n", tarefa.nome)
-		fmt.Printf("Descrição: %s\n", tarefa.descricao)
-		fmt.Printf("Data de criação: %s\n", tarefa.dataDeCriacao.Format("02/01/2006"))
-		fmt.Println("----------------------------------------")
+		formataApresentecao(&tarefa)
 	}
 }
 
-// Read
 func listaUmaTarefa(listaDeTarefas []Tarefa) *Tarefa {
+	println()
+
 	fmt.Print("Digite o ID da tarefa a ser listada: ")
+
 	leitor := bufio.NewReader(os.Stdin)
 	idStr, _ := leitor.ReadString('\n')
 	idStr = strings.TrimSpace(idStr)
 	id, _ := strconv.Atoi(idStr)
 
+	println()
+
 	for _, tarefa := range listaDeTarefas {
 		if tarefa.id == id {
-			fmt.Println(tarefa.id)
-			fmt.Println(tarefa.nome)
-			fmt.Println(tarefa.descricao)
-			fmt.Println(tarefa.dataDeCriacao)
+			formataApresentecao(&tarefa)
+			return &tarefa
 		}
 	}
 
+	fmt.Println("Nenhuma tarefa encontrada com o ID especificado.")
 	return nil
 }
 
-// Delete
+func editaTarefa(listaDeTarefas *[]Tarefa) {
+	println()
+	fmt.Print("Digite o ID da tarefa a ser editada: ")
+	leitor := bufio.NewReader(os.Stdin)
+	idStr, _ := leitor.ReadString('\n')
+	idStr = strings.TrimSpace(idStr)
+	id, _ := strconv.Atoi(idStr)
+	println()
+
+	for {
+		fmt.Println("Selecione uma opção:")
+		fmt.Println("1. Editar o nome")
+		fmt.Println("2. Editar a descrição")
+		fmt.Println("3. Editar nome e descrição")
+		fmt.Println("4. Sair")
+		fmt.Print("Opção: ")
+
+		opcao := lerOpcao()
+		switch opcao {
+		case 1, 2, 3:
+			var nome, descricao string
+			if opcao == 1 || opcao == 3 {
+				println()
+				leitor := bufio.NewReader(os.Stdin)
+				fmt.Print("Digite o nome da tarefa: ")
+				nome, _ = leitor.ReadString('\n')
+				nome = strings.TrimSpace(nome)
+			}
+			if opcao == 2 || opcao == 3 {
+				println()
+				leitor := bufio.NewReader(os.Stdin)
+				fmt.Print("Digite a descrição da tarefa: ")
+				descricao, _ = leitor.ReadString('\n')
+				descricao = strings.TrimSpace(descricao)
+			}
+
+			for i, tarefa := range *listaDeTarefas {
+				if tarefa.id == id {
+					if opcao == 1 || opcao == 3 {
+						(*listaDeTarefas)[i].nome = nome
+					}
+					if opcao == 2 || opcao == 3 {
+						(*listaDeTarefas)[i].descricao = descricao
+					}
+					fmt.Println("Tarefa editada com sucesso!")
+					return
+				}
+			}
+		case 4:
+			println()
+			fmt.Println("Saindo...")
+			return
+		default:
+			println()
+			fmt.Println("Opção inválida. Por favor, escolha uma opção válida.")
+		}
+	}
+}
+
 func removeTarefa(listaDeTarefas *[]Tarefa) {
 	if len(*listaDeTarefas) == 0 {
 		fmt.Println("Não há tarefas cadastradas para serem removidas.")
---
+++ Pacotes_Importantes/Crud/readme.md
@@ -0,0 +1,35 @@
+# Projeto de Gerenciamento de Tarefas em Go
+
+Este é um projeto simples de gerenciamento de tarefas desenvolvido em Go. Ele permite que o usuário crie, liste, visualize, edite e remova tarefas de uma lista.
+
+## Funcionalidades
+
+O projeto oferece as seguintes funcionalidades:
+
+1. **Criar Tarefa:** Permite ao usuário criar uma nova tarefa fornecendo um nome e uma descrição.
+2. **Listar Tarefas:** Lista todas as tarefas atualmente cadastradas.
+3. **Listar uma Tarefa:** Permite ao usuário visualizar os detalhes de uma tarefa específica fornecendo seu ID.
+4. **Editar Tarefa:** Permite ao usuário editar o nome e/ou descrição de uma tarefa existente.
+5. **Excluir Tarefa:** Permite ao usuário remover uma tarefa existente fornecendo seu ID.
+6. **Sair:** Encerra o programa.
+
+## Como Executar
+
+Para executar o programa, siga estas etapas:
+
+1. Certifique-se de ter o Go instalado em sua máquina. Você pode baixá-lo e instalá-lo a partir do [site oficial do Go](https://golang.org/dl/).
+2. Clone este repositório para o seu ambiente local.
+3. Navegue até o diretório do projeto.
+4. Execute o seguinte comando no terminal:
+
+    ```
+    go run main.go
+    ```
+
+5. Siga as instruções no terminal para interagir com o programa.
+
+## Tecnologias Utilizadas
+
+- Go (Golang): Linguagem de programação utilizada para desenvolver o projeto.
+- Biblioteca Padrão do Go: Utilizada para entrada/saída, manipulação de strings, tratamento de data/hora, entre outros.
+
